3d89c8fddb573ec24437a7288af0d4acf21e3430
Sean Barton <linglither@gmail.com>
2020-04-27 08:48:29 -0600

    first commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Sean Barton on 2020-04-27.
+//  Copyright © 2020 Sean Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
